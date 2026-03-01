2f4fe320036d7389b7df338768f162f4fb77a31d
hendraedufrienz-tech <hendra.edufrienz@gmail.com>
2026-03-01 22:17:18 +0700

    upload testing

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
     <main class="site-main">
         <div class="container">
             <?php
-                // if ( have_posts() ) :
-                //     while ( have_posts() ) : the_post();
-                //         the_content();
-                //     endwhile;
-                // else :
-                //     echo '<p>No content found</p>';
-                // endif;
+                echo "hello world!";
             ?>
         </div>
     </main>
